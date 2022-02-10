31386110d0d714725727f3ac6e8607885eb1e953
NOURSAR <96560657+NOURSAR@users.noreply.github.com>
2022-02-10 03:39:07 +0000

    reviewed melon-deliveries-report

--- produce_summary.py
+++ produce_summary.py
@@ -1,40 +1,18 @@
-print("Day 1")
-the_file = open("um-deliveries-day-1.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
 
-    melon = words[0]
-    count = words[1]
-    amount = words[2]
+def produce_summary(day_number, file):
+    """ to get the produce summary report from a file """
+    print("day", day_number)
+    delivery_log = open(file)
+    for line in delivery_log:
+       line = line.rstrip()
+       words = line.split('|')
 
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
+       melon = words[0]
+       count = words[1]
+       amount = words[2]
 
-
-print("Day 2")
-the_file = open("um-deliveries-day-2.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[1]
-    amount = words[2]
-
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
-
-
-print("Day 3")
-the_file = open("um-deliveries-day-3.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[1]
-    amount = words[2]
-
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
+       print(f"Delivered {count} {melon}s for total of ${amount}")
+    delivery_log.close()
+produce_summary(1, "um-deliveries-day-1.txt")
+produce_summary(2, "um-deliveries-day-2.txt")
+produce_summary(3, "um-deliveries-day-3.txt")
